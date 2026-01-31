c5dc5ed227000a30ef7df85d7a507e0612a64f86
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2026-01-31 16:25:15 +0100

    Add file browser

---
+++ src/Config/Schemas/LktFileEntities.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Lkt\FileBrowser\Config\Schemas;
+
+use Lkt\Factory\Schemas\Fields\AssocJSONField;
+use Lkt\Factory\Schemas\Fields\DateTimeField;
+use Lkt\Factory\Schemas\Fields\FileField;
+use Lkt\Factory\Schemas\Fields\ForeignKeysField;
+use Lkt\Factory\Schemas\Fields\IdField;
+use Lkt\Factory\Schemas\Fields\StringChoiceField;
+use Lkt\Factory\Schemas\Fields\StringField;
+use Lkt\Factory\Schemas\InstanceSettings;
+use Lkt\Factory\Schemas\Schema;
+use Lkt\FileBrowser\Enums\FileEntityType;
+use Lkt\Instances\LktFileEntity;
+
+return Schema::table('lkt_file_entities', LktFileEntity::COMPONENT)
+    ->setInstanceSettings(
+        InstanceSettings::define(LktFileEntity::class)
+            ->setNamespaceForGeneratedClass('Lkt\Generated')
+            ->setWhereStoreGeneratedClass(__DIR__ . '/../../Generated')
+    )
+    ->setItemsPerPage(20)
+    ->setCountableField('id')
+    ->setRelatedAccessPolicy([
+        'id' => 'value',
+        'component' => 'label',
+        'id',
+        'type',
+        'config',
+        'src',
+        'name',
+        'nameData',
+        'children',
+    ])
+    ->addField(IdField::define('id'))
+    ->addField(
+        DateTimeField::define('createdAt', 'created_at')
+            ->setDefaultReadFormat('Y-m-d')
+            ->setCurrentTimeStampAsDefaultValue()
+    )
+    ->addField(
+        DateTimeField::define('updatedAt', 'updated_at')
+            ->setDefaultReadFormat('Y-m-d')
+            ->setCurrentTimeStampAsDefaultValue()
+    )
+    ->addField(StringChoiceField::choice(FileEntityType::Types, 'type'))
+    ->addField(
+        FileField::define('src')
+        ->setStorePath([LktFileEntity::class, 'getSchemaStorePath'])
+        ->setPublicPath([LktFileEntity::class, 'getSchemaPublicPath'])
+    )
+    ->addField(AssocJSONField::define('config'))
+    ->addField(
+        ForeignKeysField::defineRelation(LktFileEntity::COMPONENT, 'children')
+    )
+    ->addField(StringField::define('name')->setIsI18nJson())
+    ->addField(AssocJSONField::define('nameData', 'name')->setIsI18nJson())
+    ;
---
+++ src/Instances/LktFileEntity.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Lkt\Instances;
+
+use Lkt\Factory\Schemas\Schema;
+use Lkt\Generated\GeneratedLktFileEntity;
+use Lkt\Http\Response;
+use Lkt\MIME;
+
+class LktFileEntity extends GeneratedLktFileEntity
+{
+    const COMPONENT = 'lkt-file-entity';
+
+    public static $schemaStorePath = null;
+    public static $schemaPublicPath = null;
+
+    public function read()
+    {
+        $fields = Schema::get(static::COMPONENT)->getAllFields();
+        return $this->readFields($fields);
+    }
+
+    public function doCreate(array $data): static
+    {
+        LktFileEntity::feedInstance($this, $data, 'create');
+        $this->save();
+
+        if ($data['parent']) {
+            $parent = static::getInstance($data['parent']);
+            $parent->setChildren([...$parent->getChildrenIds(), $this->getId()])->save();
+        }
+
+        return $this;
+    }
+
+    public function doUpdate(array $data): static
+    {
+        LktFileEntity::feedInstance($this, $data, 'update');
+        return $this->save();
+    }
+
+    public function getSrcResponse(): Response
+    {
+        $path = static::getSchemaStorePath($this);
+        if (!$path) return Response::notFound();
+
+        $fileName = $this->getSrcName();
+        if (!$fileName) return Response::notFound();
+
+        $file = $this->getSrc();
+        $content = file_get_contents($file->path);
+        return Response::ok($content)
+            ->setContentTypeMIME(MIME::getByExtension(pathinfo($fileName, PATHINFO_EXTENSION)))
+            ->enableCacheToOneYear()
+            ;
+    }
+
+
+    public static function getSchemaStorePath($instance): string
+    {
+        if (is_callable(static::$schemaStorePath)) {
+            return call_user_func(static::$schemaStorePath, $instance);
+        }
+        return '';
+    }
+
+
+    public static function getSchemaPublicPath(LktFileEntity|null $instance = null): string
+    {
+        if ($instance instanceof LktFileEntity) {
+            if ($instance->typeIsUnit() || $instance->typeIsDir()) return '';
+        }
+
+        if (is_callable(static::$schemaPublicPath)) {
+            return call_user_func(static::$schemaPublicPath, $instance);
+        }
+        return '';
+    }
+}

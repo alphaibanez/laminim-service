2fdd1b215441a9d2cace8ec0a38d69e0fc187fff
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2026-05-04 18:55:35 +0200

    Add Revolut Webhook Events

--- src/Connectors/Enums/RevolutWebhookEvent.php
+++ src/Connectors/Enums/RevolutWebhookEvent.php
@@ -7,6 +7,11 @@ enum RevolutWebhookEvent: string
     case OrderCompleted = 'ORDER_COMPLETED';
     case OrderAuthorised = 'ORDER_AUTHORISED';
     case OrderCanceled = 'ORDER_CANCELLED';
+    case OrderFailed = 'ORDER_FAILED';
+    case OrderIncrementalAuthorisationAuthorised = 'ORDER_INCREMENTAL_AUTHORISATION_AUTHORISED';
+    case OrderIncrementalAuthorisationDeclined = 'ORDER_INCREMENTAL_AUTHORISATION_DECLINED';
+    case OrderIncrementalAuthorisationFailed = 'ORDER_INCREMENTAL_AUTHORISATION_FAILED';
+    case OrderIncrementalAuthorisationChanged = 'ORDER_PAYMENT_AUTHENTICATION_CHALLENGED';
     case OrderPaymentAuthenticated = 'ORDER_PAYMENT_AUTHENTICATED';
     case OrderPaymentDeclined = 'ORDER_PAYMENT_DECLINED';
     case OrderPaymentFailed = 'ORDER_PAYMENT_FAILED';
@@ -17,4 +22,8 @@ enum RevolutWebhookEvent: string
     case DisputeActionReview = 'DISPUTE_UNDER_REVIEW';
     case DisputeActionWon = 'DISPUTE_WON';
     case DisputeActionLost = 'DISPUTE_LOST';
+    case SubscriptionInitiated = 'SUBSCRIPTION_INITIATED';
+    case SubscriptionFinished = 'SUBSCRIPTION_FINISHED';
+    case SubscriptionCancelled = 'SUBSCRIPTION_CANCELLED';
+    case SubscriptionOverdue = 'SUBSCRIPTION_OVERDUE';
 }

dde982c0d05eb9995ce44742aebc21b3d2ec9f45
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2026-05-07 20:24:30 +0200

    Update coupon config

--- src/Config/Schemas/LktShoppingCoupons.php
+++ src/Config/Schemas/LktShoppingCoupons.php
@@ -9,11 +9,17 @@ use Lkt\Factory\Schemas\Fields\FloatField;
 use Lkt\Factory\Schemas\Fields\ForeignKeyField;
 use Lkt\Factory\Schemas\Fields\IdField;
 use Lkt\Factory\Schemas\Fields\IntegerChoiceField;
+use Lkt\Factory\Schemas\Fields\PivotField;
+use Lkt\Factory\Schemas\Fields\PivotLeftIdField;
+use Lkt\Factory\Schemas\Fields\PivotPositionField;
+use Lkt\Factory\Schemas\Fields\PivotRightIdField;
 use Lkt\Factory\Schemas\Fields\StringField;
 use Lkt\Factory\Schemas\InstanceSettings;
 use Lkt\Factory\Schemas\Schema;
 use Lkt\Instances\LktCurrency;
 use Lkt\Instances\LktShoppingCoupon;
+use Lkt\Instances\LktShoppingOrder;
+use Lkt\Instances\LktShoppingOrderPivotCoupon;
 use Lkt\Instances\LktUser;
 use Lkt\Shop\Enums\CouponDiscountType;
 use Lkt\Shop\Enums\CouponType;
@@ -56,4 +62,11 @@ Schema::add(
         ->addField(FloatField::define('minimumOrderAmount', 'minimum_order_amount')->setDefaultValue(0))
         ->addField(BooleanField::define('isActive', 'is_active')->setDefaultValue(false))
         ->addField(BooleanField::define('stackable', 'stackable')->setDefaultValue(false))
+
+        ->addField(PivotField::definePivot(LktShoppingCoupon::COMPONENT, 'lkt_shopping_orders__coupons', 'orders', 'coupon_id', LktShoppingOrderPivotCoupon::COMPONENT)
+            ->setPivotLeftIdField(PivotLeftIdField::defineRelation(LktShoppingOrder::COMPONENT, 'order', 'order_id'))
+            ->setPivotRightIdField(PivotRightIdField::defineRelation(LktShoppingCoupon::COMPONENT, 'coupon', 'coupon_id'))
+            ->setPivotPositionField(PivotPositionField::define('position'))
+            ->setPivotInstanceConfig(LktShoppingOrderPivotCoupon::class, 'Lkt\Generated', __DIR__ . '/../../Generated')
+        )
 );

fa7384264deb7834bc76af72d4446893631c9a29
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2026-09-06 11:15:04 +0200

    Update fields deprecations

--- src/Factory/Schemas/Fields/UnixTimeStampField.php
+++ src/Factory/Schemas/Fields/UnixTimeStampField.php
@@ -2,6 +2,7 @@
 
 namespace Lkt\Factory\Schemas\Fields;
 
+/** @deprecated Use DateTimeField::unixTimeStamp instead */
 class UnixTimeStampField extends DateTimeField
 {
     public static function define(string $name, string $column = ''): static
